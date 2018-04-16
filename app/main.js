7cb75ccb851458151bba9f5769bcab2b97968107
Ronald Alfaro <ronald.alfaro91@gmail.com>
2018-04-16 13:29:01 -0600

    New page init
    Deleted old files and added new files

--- app/main.js
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var HelloWorld = React.createClass({  render: function () {    
-  return React.createElement('div', null, 'Hello World 3'); 
-}});
-
-ReactDOM.render(React.createElement(HelloWorld), document.querySelector('#react-mount'));
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entry: {
-    app:['./app/main.js']
-  },
-  output:{
-    path: './app',
-    filename: 'bundle.js'
-  }
-};
