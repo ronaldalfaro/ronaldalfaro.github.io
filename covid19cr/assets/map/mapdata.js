56a7c2413316017159733a688dd45f63339a17f1
Ronald Alfaro B <ronald.alfaro91@gmail.com>
2020-03-22 15:29:56 -0600

    Covid19cr
    subida de primer index

---
+++ covid19cr/assets/map/mapdata.js
@@ -0,0 +1,263 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "Cantidad 0 0%",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Casos activos: 1 0.8547008547%",
+    location_url: "",
+    location_color: "#f7d708",
+    location_opacity: "1",
+    location_hover_opacity: 1,
+    location_size: "25",
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    CRI1327: {
+      name: "Cartago",
+      description: "Cantidad 9 7.692307692%"
+    },
+    CRI1328: {
+      name: "Heredia",
+      description: "Cantidad 11 9.401709402%"
+    },
+    CRI1329: {
+      name: "Limón",
+      description: "Cantidad 1 0.8547008547%"
+    },
+    CRI1330: {
+      name: "Puntarenas"
+    },
+    CRI1331: {
+      name: "San José",
+      description: "Cantidad 53 45.2991453%"
+    },
+    CRI1333: {
+      name: "Alajuela",
+      description: "Cantidad 38 32.47863248%"
+    },
+    CRI1334: {
+      name: "Guanacaste",
+      description: "Cantidad 5 4.273504274%"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "9.920878",
+      lng: "-84.145945",
+      name: "Escazú",
+      color: "#ff9e00",
+      size: "50",
+      description: "Casos activos: 11 9.401709402%"
+    },
+    "1": {
+      lat: "10.015910",
+      lng: "-84.214373",
+      name: "Alajuela",
+      color: "#ce0000",
+      size: "75",
+      description: "Casos activos: 25 21.36752137%"
+    },
+    "2": {
+      lat: "9.933333",
+      lng: "-84.083333",
+      name: "San José",
+      color: "#ff9e00",
+      size: "50",
+      description: "Casos activos: 12 10.25641026%"
+    },
+    "3": {
+      lat: "9.931379",
+      lng: "-84.180798",
+      name: "Santa Ana",
+      description: "Casos activos: 7 5.982905983%"
+    },
+    "4": {
+      lat: "9.917229",
+      lng: "-84.03501",
+      name: "Curridabat",
+      description: "Casos activos: 5 4.273504274%"
+    },
+    "5": {
+      lat: "10.073655",
+      lng: "-84.313632",
+      name: "Grecia",
+      description: "Casos activos: 5 4.273504274%"
+    },
+    "6": {
+      lat: "9.923775",
+      lng: "-84.000852",
+      name: "La Unión",
+      description: "Casos activos: 5 4.273504274%"
+    },
+    "7": {
+      lat: "10.073097",
+      lng: "-84.245015",
+      name: "Poás",
+      description: "Casos activos: 5 4.273504274%"
+    },
+    "8": {
+      lat: "9.864686",
+      lng: "-83.920386",
+      name: "Cartago",
+      description: "Casos activos: 4 3.418803419%"
+    },
+    "9": {
+      lat: "9.897177",
+      lng: "-84.064862",
+      name: "Desamparados",
+      description: "Casos activos: 4 3.418803419%"
+    },
+    "10": {
+      lat: "9.99835",
+      lng: "-84.120691",
+      name: "Heredia",
+      description: "Casos activos: 4 3.418803419%"
+    },
+    "11": {
+      lat: "9.963499",
+      lng: "-84.047514",
+      name: "Moravia",
+      description: "Casos activos: 4 3.418803419%"
+    },
+    "12": {
+      lat: "9.937177",
+      lng: "-84.040615",
+      name: "Montes de Oca",
+      description: "Casos activos: 3 2.564102564%"
+    },
+    "13": {
+      lat: "10.144688",
+      lng: "-85.453249",
+      name: "Nicoya",
+      description: "Casos activos: 3 2.564102564%"
+    },
+    "14": {
+      lat: "9.376858",
+      lng: "-83.661771",
+      name: "Perez Zeledón",
+      description: "Casos activos: 3 2.564102564%"
+    },
+    "15": {
+      lat: "10.02665",
+      lng: "-84.127484",
+      name: "San Pablo",
+      description: "Casos activos: 3 2.564102564%"
+    },
+    "16": {
+      lat: "9.977026",
+      lng: "-84.007778",
+      name: "Coronado",
+      description: "Casos activos: 2 1.709401709%"
+    },
+    "17": {
+      lat: "10.514004",
+      lng: "-84.433104",
+      name: "San Carlos",
+      description: "Casos activos: 2 1.709401709%"
+    },
+    "18": {
+      lat: "10.260717",
+      lng: "-85.585986",
+      name: "Santa Cruz",
+      description: "Casos activos: 2 1.709401709%"
+    },
+    "19": {
+      lat: "9.958463",
+      lng: "-84.081443",
+      name: "Tibás",
+      description: "Casos activos: 2 1.709401709%"
+    },
+    "20": {
+      lat: "9.97838",
+      lng: "-84.379721",
+      name: "Atenas"
+    },
+    "21": {
+      lat: "10.019366",
+      lng: "-84.124257",
+      name: "Barva"
+    },
+    "22": {
+      lat: "9.992131",
+      lng: "-83.040035",
+      name: "Limón"
+    },
+    "23": {
+      lat: "10.014274",
+      lng: "-84.098486",
+      name: "San Rafael"
+    },
+    "24": {
+      lat: "10.037298",
+      lng: "-84.159008",
+      name: "Santa Barbara"
+    },
+    "25": {
+      lat: "9.980734",
+      lng: "-84.091177",
+      name: "Santo Domingo"
+    }
+  },
+  labels: {},
+  regions: {}
+};
